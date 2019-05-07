c88138de0fe5bca84f2b37ab662dd823ee2bfacb
saumitra1993 <theapogee2011@gmail.com>
2019-05-07 11:12:34 +0530

    create home route

---
+++ src/components/pages/Landing/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+
+export default class LandingPage extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return <h1>Hello World Proso.io!</h1>
+  }
+}
