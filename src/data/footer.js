366e0a9c39256fa78471783b07d7006dcd6c1127
Harsh Yelne <39940022+webbyharsh@users.noreply.github.com>
2019-05-09 17:57:02 +0530

    footer data (can be modified later  )

---
+++ src/data/footer.js
@@ -0,0 +1,32 @@
+const arr = [
+  {
+    title: "Seva Partners",
+    children: [
+      { icon: "", label: "Proso.io", link: "#" },
+      { icon: "", label: "Giving Tuesday India", link: "#" },
+      { icon: "", label: "Seva Kitchen food sharing app", link: "#" }
+    ]
+  },
+  {
+    title: "Seva Stats",
+    children: [{ label: "1234 Donors" }, { label: "560 Receiver Volunteers" }]
+  },
+  {
+    title: "Social links",
+    children: [
+      { icon: "fa fa-whatsapp", label: "WhatsApp", link: "#" },
+      { icon: "fa fa-instagram", label: "Instagram", link: "#" },
+      { icon: "fa fa-facebook", label: "Facebook", link: "#" }
+    ]
+  },
+
+  {
+    title: "Seva Locations",
+    children: [
+      { link: "#", label: "More than hundred hospitals" },
+      { link: "#", label: "50 Schools" }
+    ]
+  }
+];
+
+export default arr;
