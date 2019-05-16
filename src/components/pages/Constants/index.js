eb0aae752e28de11686c4f7fedc9ce144ec6457f
Harsh Yelne <39940022+webbyharsh@users.noreply.github.com>
2019-05-16 23:16:56 +0530

    link data for LandingSectionTwo page

---
+++ src/components/pages/Constants/index.js
@@ -0,0 +1,15 @@
+const feature_data = [
+    { "img": "/images/Image8.png", "link": "#" },
+    { "img": "/images/Image9.png", "link": "#" },
+    { "img": "/images/Image10.png", "link": "#" },
+    { "img": "/images/Image11.png", "link": "#" },
+    { "img": "/images/Image12.png", "link": "#" },
+    { "img": "/images/Image13.png", "link": "#" },
+    { "img": "/images/Image14.png", "link": "#" },
+    { "img": "/images/Image15.png", "link": "#" },
+    { "img": "/images/Image16.png", "link": "#" },
+    { "img": "/images/Image17.png", "link": "#" },
+    { "img": "/images/Image18.png", "link": "#" },
+    { "img": "/images/Image19.png", "link": "#" },
+]
+export default feature_data;
