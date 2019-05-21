92fdb4e2ee0433caefd4510317f355b377a0ccb1
saumitra1993 <theapogee2011@gmail.com>
2019-05-21 10:50:44 +0530

    Revert "Harsh/landing section two"

---
+++ src/components/pages/Landing/LandingSectionTwo.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+
+export default class LandingSectionTwo extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return <h1>Stub for landing section one</h1>
+  }
+}
