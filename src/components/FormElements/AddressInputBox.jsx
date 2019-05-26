6145051fd738689aa17897540dc446fa1bec03f5
Harsh Yelne <39940022+webbyharsh@users.noreply.github.com>
2019-05-26 18:32:29 +0530

    Create AddressInputBox.jsx

---
+++ src/components/FormElements/AddressInputBox.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './AddressInputBox.css'
+import keys from '../config';
+
+class AddressInputBox extends Component {
+    state = {
+        value: '',
+        predictions: []
+    }
+    handleChange = (e) => {
+        this.setState({ value: e.target.value })
+        fetch('https://maps.googleapis.com/maps/api/place/autocomplete/json?input=' + e.target.value + '&key=' + keys.apikey2)
+            .then((res) => res.json())
+            .then((result) => {
+                this.setState({
+                    predictions: result.predictions
+                })
+            })
+    }
+    render() {
+        return (
+            <div className="autocomplete">
+                <label htmlFor="address">Your Location (we'll use this to connect you to chapters in your area)</label>
+                <input name="address" id="address" list="data-list" type="text" onChange={this.handleChange} placeholder="Start Typing"></input>
+                <div className="predictions">
+                    <datalist id="data-list" className="results">
+                        {this.state.predictions.map((children) => {
+                            return (<option key={children.id}>{children.description}</option>)
+                        })}
+                    </datalist>
+                </div>
+            </div>);
+    }
+}
+
+export default AddressInputBox;
