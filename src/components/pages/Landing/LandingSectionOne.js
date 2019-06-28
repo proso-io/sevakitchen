a7059abe4faf7cfd267f5909fbfc99d8b3a2fecd
Shivam <shivam270295@gmail.com>
2019-06-29 02:20:00 +0530

    Make landing section 1

--- src/components/pages/Landing/LandingSectionOne.js
+++ src/components/pages/Landing/LandingSectionOne.js
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
-import InitiativeCard from "../../InitiativeCard";
+import InitiativeGroup from "../../InitiativeGroup";
+import "../../../styles/LandingSectionOne.scss";
 
 
 export default class LandingSectionOne extends Component {
@@ -9,9 +10,25 @@ export default class LandingSectionOne extends Component {
 
   render(){
     return (
-      <div>
-        <h1>Stub for landing section two</h1>
-        <InitiativeCard img="/images/sevatiffin.jpeg" title="Seva Tiffin" description="Food is served at the several locations for the needy at Hospitals, Schools for underprivileged children, Old age Homes. If you wish to serve at any of these locations you can Sign Up for Seva. You can also buy a meal for the location which will be delivered to the needy by Volunteers." />
+      <div className="landing-section-1">
+      <div className="trapezium">
+        <div className="triangle" />
+        <div className="rect"/>
+      </div>
+        <div className="content">
+          <h1>SERVING A PLATEFUL OF LOVE</h1>
+          <h4 className="tagline">Seva kitchen helps feed the needy through it's various initiatives</h4>
+          <InitiativeGroup
+            data = {[
+              {img: "/images/headericon.jpg", title: "Title1", description: "Desription 1"},
+              {img: "/images/headericon.jpg", title: "Title2", description: "Desription 1"},
+              {img: "/images/headericon.jpg", title: "Title3", description: "Desription 1"},
+              {img: "/images/headericon.jpg", title: "Title4", description: "Desription 1"},
+              {img: "/images/headericon.jpg", title: "Title5", description: "Desription 1"},
+              {img: "/images/headericon.jpg", title: "Title6", description: "Desription 1"}
+            ]}
+            />
+        </div>
       </div>
     )
   }
